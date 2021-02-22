50365abf37034be3c738865d7eab07d1eb496f46
B0B44 <roman.bobalo@gmail.com>
2021-02-22 20:28:42 +0200

    lab1 optimizations

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -3,17 +3,25 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int SAMPLE_COUNT = 10000;
+int SAMPLE_COUNT_FOR_AVG = 5;
+int RUNS_STEP = 100;
+int THREADS_ARRAY[5] = {1,2,4,6};
+int THREADS_ARRAY_LENGTH = 4;
+
+double a[10000][10000];
+double b[10000];
+double c[10000];
 
 double run(int n, int threads, bool output)
 {
 	double start_time = omp_get_wtime();
-	
+
 	int i, j, thread_num;
 
 	omp_set_num_threads(threads);
 
-	// init matrix
-	double a[n][n];
+	// init matrix & vector
 	#pragma omp parallel shared(n, threads), private(i, j, thread_num)
 	{
 		thread_num = omp_get_thread_num();
@@ -21,46 +29,19 @@ double run(int n, int threads, bool output)
 
 		while (i < n){
 			j = thread_num;
+			double sin_i = sin(i);
 			while (j < n){
-				a[i][j] = sin(i) + cos(j);
+				a[i][j] = sin_i + cos(j);
 				j += threads;
+			}
+
+			if ((thread_num != 0 && i % thread_num == 0) || (i == 0 && thread_num == 0)){
+				b[i] = sin_i * cos(i);
 			}
 			i++;
 		}
 	}
 
-	if (output){
-		printf("Matrix A\n\n");
-		for(i = 0; i < n; i++){
-			for(j = 0; j < n; j++){
-				printf("%f  ", a[i][j]);
-			}
-			printf("\n");
-		}
-		printf("\n\n");
-	}
-
-	double b[n];
-	#pragma omp parallel shared(n, threads), private(i, thread_num)
-	{
-		thread_num = omp_get_thread_num();
-		i = thread_num;
-
-		while (i < n){
-			b[i] = sin(i) * cos(i);
-			i += threads;
-		}
-	}
-
-	if (output){
-		printf("Vector B\n\n");
-		for(i = 0; i < n; i++){
-			printf("%f  ", b[i]);
-		}
-		printf("\n\n");
-	}
-
-	double c[n];
 	double result;
 	#pragma omp parallel shared(n, threads), private(i, j, thread_num, result)
 	{
@@ -79,26 +60,29 @@ double run(int n, int threads, bool output)
 		}
 	}
 
-	if (output){
-		printf("Result C\n\n");
-		for(i = 0; i < n; i++){
-			printf("%f  ", c[i]);
-		}
-		printf("\n\n");
-	}
-
 	return omp_get_wtime() - start_time;;
 }
 
+double avrg(int runs, int i, int j){
+	double results = 0;
+	for(int x = 0; x < 10; x++){
+		results += run(i, j, false);
+	}
+	return results / runs;
+}
 
 int main(int argc, char *argv[])
 {
 	int max_threads = omp_get_max_threads();
-	for(int i = 1; i <= 1000; i++){
+	
+	for(int i = 0; i <= SAMPLE_COUNT; i+=RUNS_STEP){
 		printf("%d ", i);
-		for(int j = 1; j <= max_threads; j++){
-			printf("%f ", run(i, j, false));
+
+		for(int j = 0; j < THREADS_ARRAY_LENGTH; j++){
+			printf("%f ", avrg(SAMPLE_COUNT_FOR_AVG, i, THREADS_ARRAY[j]));
+			
 		}
+		
 		printf("\n");
 
 	}
